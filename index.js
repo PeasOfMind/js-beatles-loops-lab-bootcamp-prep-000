e04b95af90ad5ba1ecde39085a87b73893dc534d
Yeqing Liu <sunsetpeak42@gmail.com>
2018-07-13 14:58:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add solution here
 function theBeatlesPlay(musicians,instruments){
-  beatlesArr = [];
+  let beatlesArr = [];
   for(let i=0; i<musicians.length; i++){
     beatlesArr.push(`${musicians[i]} plays ${instruments[i]}`)
   }

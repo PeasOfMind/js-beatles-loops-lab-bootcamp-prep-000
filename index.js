e43d72dc7c31bb9c5413356e7db8257cd78e140e
Yeqing Liu <sunsetpeak42@gmail.com>
2018-07-13 14:57:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // add solution here
 function theBeatlesPlay(musicians,instruments){
-  
+  beatlesArr = [];
+  for(let i=0; i<musicians.length; i++){
+    beatlesArr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return beatlesArr;
 }
